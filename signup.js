747d164350742b1e6a6acf2e027eb9e82c3c6f9e
RajviM7 <rajvi.m7@gmail.com>
2017-08-18 09:09:43 -0400

    idk

---
+++ signup.js
@@ -0,0 +1,9 @@
+// window.alert("OKAY");
+
+var username = document.getElementById("username");
+var password = document.getElementById("password");
+var submit = document.getElementbyId("submit");
+
+function submitClick() {
+  window.alert("working");
+}
